b7e809f96fc958bb0d8fdef3c7e02b40e4d919d4
Sean P. Kane <sean@superorbital.io>
2023-10-19 12:11:58 -0700

    Force go tests to ignore directories named start

--- go-test-repo-mod.sh
+++ go-test-repo-mod.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 cmd=(go test)
+skip_start="true"
 . "$(dirname "${0}")/lib/cmd-repo-mod.bash"
--- lib/cmd-repo-mod.bash
+++ lib/cmd-repo-mod.bash
@@ -12,17 +12,33 @@ export GO111MODULE=on
 error_code=0
 # Assume parent folder of go.mod is module root folder
 #
-for sub in $(find . -name go.mod -not -path '*/vendor/*' -exec dirname "{}" ';' | sort -u); do
-	pushd "${sub}" > /dev/null || exit 1
-	if [ "${error_on_output:-}" -eq 1 ]; then
-		output=$(/usr/bin/env "${ENV_VARS[@]}" "${cmd[@]}" "${OPTIONS[@]}" 2>&1)
-		if [ -n "${output}" ]; then
-			printf "%s\n" "${output}"
+if [ -z ${skip_start+x} ]; then
+	for sub in $(find . -name go.mod -not -path '*/vendor/*' -not -path '*/start/*' -exec dirname "{}" ';' | sort -u); do
+		pushd "${sub}" > /dev/null || exit 1
+		if [ "${error_on_output:-}" -eq 1 ]; then
+			output=$(/usr/bin/env "${ENV_VARS[@]}" "${cmd[@]}" "${OPTIONS[@]}" 2>&1)
+			if [ -n "${output}" ]; then
+				printf "%s\n" "${output}"
+				error_code=1
+			fi
+		elif ! /usr/bin/env "${ENV_VARS[@]}" "${cmd[@]}" "${OPTIONS[@]}"; then
 			error_code=1
 		fi
-	elif ! /usr/bin/env "${ENV_VARS[@]}" "${cmd[@]}" "${OPTIONS[@]}"; then
-		error_code=1
-	fi
-	popd > /dev/null || exit 1
-done
+		popd > /dev/null || exit 1
+	done
+else
+	for sub in $(find . -name go.mod -not -path '*/vendor/*' -not -path '*/start/*' -exec dirname "{}" ';' | sort -u); do
+		pushd "${sub}" > /dev/null || exit 1
+		if [ "${error_on_output:-}" -eq 1 ]; then
+			output=$(/usr/bin/env "${ENV_VARS[@]}" "${cmd[@]}" "${OPTIONS[@]}" 2>&1)
+			if [ -n "${output}" ]; then
+				printf "%s\n" "${output}"
+				error_code=1
+			fi
+		elif ! /usr/bin/env "${ENV_VARS[@]}" "${cmd[@]}" "${OPTIONS[@]}"; then
+			error_code=1
+		fi
+		popd > /dev/null || exit 1
+	done
+fi
 exit $error_code
